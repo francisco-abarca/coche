ae8a476d5c5ca26562b2d87f4942a2e1a1a9b53d
francisco-abarca <119293082+francisco-abarca@users.noreply.github.com>
2023-05-01 19:44:17 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,76 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(receivedNumber * 1000)
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(500)
+})
+input.onGesture(Gesture.Shake, function () {
+    dado = randint(1, 6)
+    if (dado == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (dado == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (dado == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (dado == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (dado == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+    radio.sendNumber(dado)
+})
+let dado = 0
+dado = 0
+radio.setGroup(1)
